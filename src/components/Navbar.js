9f641ee5e40d8396d03f4997aa12388df11a64c5
tusharginotra <tusharginotra2000@gmail.com>
2022-03-24 10:43:43 +0530

    navbar is done
    adding daily news

---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+export class Navbar extends Component {
+  
+
+  render() {
+    return (
+      <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+            <Link className="navbar-brand" to="/">Daily News</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                </li>
+                
+                <li className="nav-item">
+                <Link className="nav-link active" to="/entertainment">Entertainment</Link>
+                </li>
+                
+                <li className="nav-item">
+                <Link className="nav-link active" to="/sports">Sports</Link>
+                </li>
+                <li className="nav-item">
+                <Link className="nav-link active" to="/business">Business</Link>
+                </li>
+                <li className="nav-item">
+                <Link className="nav-link active" to="/general">General</Link>
+                </li>
+                <li className="nav-item">
+                <Link className="nav-link active" to="/health">Health</Link>
+                </li>
+                <li className="nav-item">
+                <Link className="nav-link active" to="/science">Science</Link>
+                </li>
+                <li className="nav-item">
+                <Link className="nav-link active" to="/technology">Technology</Link>
+                </li>
+                
+            </ul>
+            </div>
+        </div>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Navbar
---
+++ src/components/NewsItem.js
@@ -0,0 +1,23 @@
+//import { getByTitle } from '@testing-library/react'
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+   
+  render() {
+    let {title,description,imageUrl,newsUrl} = this.props;
+    return (
+      <div className="my-4"> 
+      <div className="card" >
+        <img src={imageUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read more</a>
+        </div>
+      </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
